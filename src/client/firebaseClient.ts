c5526a70fe513f1975752f7d92d19c5507582351
Evan.Mateo <gevanmateo@gmail.com>
2019-11-17 13:22:37 -0500

    typescript config

---
+++ src/client/firebaseClient.ts
@@ -0,0 +1,15 @@
+declare const firebase;
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBY0UgjU--j4MOG8pOhnqvoU6x6nCSOCU8",
+    authDomain: "chatapp-58582.firebaseapp.com",
+    databaseURL: "https://chatapp-58582.firebaseio.com",
+    projectId: "chatapp-58582",
+    storageBucket: "",
+    messagingSenderId: "522991633490",
+    appId: "1:522991633490:web:303179d4fdf49654"
+  };
+
+export const firebaseInit = firebase.initializeApp(firebaseConfig);
+
+console.log("Initialized FirebaseApp!");
