b11c59333d0d1955463ee90ee12b48ed2fb542b3
EvanMateo7 <gevanmateo@gmail.com>
2021-08-08 04:58:08 -0400

    Remove vertical prop from UserAvatar component

--- src/client/components/UserAvatar.tsx
+++ src/client/components/UserAvatar.tsx
@@ -4,14 +4,12 @@ import Typography from "@material-ui/core/Typography";
 import React from "react";
 import { User } from '../../models';
 
-export interface UserProps { user: User, vertical?: boolean }
+export interface UserProps { user: User }
 
-export const UserAvatar = ({vertical = false, ...props}: UserProps) => {
-
-  const direction = vertical ? 'column' : 'row';
+export const UserAvatar = (props: UserProps) => {
 
   return (
-    <Box display="flex" flexDirection={direction} justifyContent="center" alignItems="center" gridGap="5px" maxWidth={200}>
+    <Box display="flex" justifyContent="center" alignItems="center" gridGap="5px" maxWidth={200}>
       <Avatar alt={props.user?.name} src={props.user?.photoURL} />
       <Typography variant="body2" noWrap>{props.user?.name}</Typography>
     </Box>
