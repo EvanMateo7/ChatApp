5a45417b45fe8184d27d481e22bc795c0fa5a834
EvanMateo7 <gevanmateo@gmail.com>
2021-10-11 23:15:45 -0400

    Refactor ProfileEditOpener to use wrapping child in clickable element instead of copying child and overwritting onClick prop

--- src/client/components/ProfileEditOpener.tsx
+++ src/client/components/ProfileEditOpener.tsx
@@ -1,8 +1,8 @@
-import React, { FunctionComponent, ReactElement, cloneElement } from 'react';
+import React, { FunctionComponent, ReactElement } from 'react';
 import { ProfileEdit } from './ProfileEdit';
 import { User } from '../../models';
 
-interface ProfileEditOpenerProps { user: User, children: ReactElement<{ onClick: any }> }
+interface ProfileEditOpenerProps { user: User}
 
 export const ProfileEditOpener: FunctionComponent<ProfileEditOpenerProps> = (props) => {
 
@@ -18,12 +18,9 @@ export const ProfileEditOpener: FunctionComponent<ProfileEditOpenerProps> = (pro
 
   return (
     <>
-      {cloneElement(props.children, {
-        onClick: () => {
-          props.children.props.onClick && props.children.props.onClick()
-          handleClickOpen();
-        }
-      })}
+      <span style={{cursor: "pointer"}} onClick={handleClickOpen}>
+        {props.children}
+      </span>
       {open && <ProfileEdit user={props.user} open={open} handleClose={handleClose} />}
     </>
   );
